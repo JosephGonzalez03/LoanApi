0c762e0f598e6b9114d635c1a759cc2182df0d64
Joe <josephdg3@gmailcom>
2020-04-04 10:14:26 -0700

    test: Add prop unit tests for getAll method

--- src/test/java/com/nerd/LoanAPI/integration/LoanServiceIntegrationTest.java
+++ src/test/java/com/nerd/LoanAPI/integration/LoanServiceIntegrationTest.java
@@ -1,17 +1,13 @@
 package com.nerd.LoanAPI.integration;
 
 import com.nerd.LoanAPI.model.Loan;
+import com.nerd.LoanAPI.model.OrderBy;
 import com.nerd.LoanAPI.model.User;
 import com.nerd.LoanAPI.repository.LoanDao;
 import com.nerd.LoanAPI.repository.UserDao;
 import com.nerd.LoanAPI.service.LoanService;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
+import static  org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class LoanServiceIntegrationTest {
@@ -48,7 +44,6 @@ public class LoanServiceIntegrationTest {
     User Joe = new User();
     Loan a = new Loan();
     Loan b = new Loan();
-    List mockedList = new ArrayList();
     List<Loan> expected = new ArrayList<>();
 
     @Before
@@ -68,22 +63,74 @@ public class LoanServiceIntegrationTest {
         b.setInterestRate(BigDecimal.valueOf(1.00));
         b.setContribution(BigDecimal.ONE);
         b.setOutstandingBalance(BigDecimal.valueOf(2000.00));
+    }
 
-        mockedList.add(a);
-        mockedList.add(b);
-
-        Mockito.when(loanDao.findByUserIdOrderByContributionDesc(1))
-                .thenReturn(mockedList);
+    @After
+    public void cleanUp() {
+        expected.clear();
     }
 
     @Test
-    public void whenFindByUserIdOrderByContribution_thenLoanListShouldBeReturned() {
-        List<Loan> found = loanDao.findByUserIdOrderByContributionDesc(1);
+    public void getAll_InterestRateOrderByEnumAs2ndParam_ReturnsLoanListInOrderOfDescendingInterestRate() {
+        when(loanDao.findByUserIdOrderByInterestRateDesc(1))
+                .thenReturn(Arrays.asList(a, b));
+
+        List<Loan> found = loanService.getAll(1, OrderBy.INTEREST_RATE);
 
         expected.add(a);
         expected.add(b);
 
         Assert.assertEquals(found.get(0), expected.get(0));
         Assert.assertEquals(found.get(1), expected.get(1));
+
+        verify(loanDao, times(1)).findByUserIdOrderByInterestRateDesc(1);
+    }
+
+    @Test
+    public void getAll_OutstandingBalanceOrderByEnumAs2ndParam_ReturnsLoanListInOrderOfDescendingOutstandingBalance() {
+        when(loanDao.findByUserIdOrderByOutstandingBalanceDesc(1))
+                .thenReturn(Arrays.asList(b, a));
+
+        List<Loan> found = loanService.getAll(1, OrderBy.OUTSTANDING_BALANCE);
+
+        expected.add(b);
+        expected.add(a);
+
+        Assert.assertEquals(found.get(0), expected.get(0));
+        Assert.assertEquals(found.get(1), expected.get(1));
+
+        verify(loanDao, times(1)).findByUserIdOrderByOutstandingBalanceDesc(1);
+    }
+
+    @Test
+    public void getAll_ContributionOrderByEnumAs2ndParam_ReturnsLoanListInOrderOfDescendingContribution() {
+        when(loanDao.findByUserIdOrderByContributionDesc(1))
+                .thenReturn(Arrays.asList(a, b));
+
+        List<Loan> found = loanService.getAll(1, OrderBy.CONTRIBUTION);
+
+        expected.add(a);
+        expected.add(b);
+
+        Assert.assertEquals(found.get(0), expected.get(0));
+        Assert.assertEquals(found.get(1), expected.get(1));
+
+        verify(loanDao, times(1)).findByUserIdOrderByContributionDesc(1);
+    }
+
+    @Test
+    public void getAll_NameOrderByEnumAs2ndParam_ReturnsLoanListOrderedAlphabetically() {
+        when(loanDao.findByUserIdOrderByName(1))
+                .thenReturn(Arrays.asList(a, b));
+
+        List<Loan> found = loanService.getAll(1, OrderBy.NAME);
+
+        expected.add(a);
+        expected.add(b);
+
+        Assert.assertEquals(found.get(0), expected.get(0));
+        Assert.assertEquals(found.get(1), expected.get(1));
+
+        verify(loanDao, times(1)).findByUserIdOrderByName(1);
     }
 }
