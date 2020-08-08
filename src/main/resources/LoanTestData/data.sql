c5089122d0c2e0aff7f2b90357e27d22adf00cde
JosephGonzalez03 <josephdg3@gmail.com>
2020-08-08 16:08:39 -0700

    test: Add test data
    Add test data for Postman contract tests.

--- src/main/resources/LoanTestData/data.sql
+++ src/main/resources/LoanTestData/data.sql
@@ -2,3 +2,7 @@ insert into users (first_name, last_name) values
     ('John', 'Doe'),
     ('Jane', 'Doe'),
     ('Joe', 'Doe');
+
+insert into loans (user_id, name, interest_rate, outstanding_balance, contribution) values
+    (1, 'car loan', 1.005, 20000.00, 30.00),
+    (1, 'house loan', 5.020, 15000.00, 20.00);
