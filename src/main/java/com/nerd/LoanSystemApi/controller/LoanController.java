0e67578f747751af2d7101f135ccc377bddefa60
JosephGonzalez03 <josephdg3@gmail.com>
2021-09-26 18:52:15 -0700

    chore: Format long lines

--- src/main/java/com/nerd/LoanSystemApi/controller/LoanController.java
+++ src/main/java/com/nerd/LoanSystemApi/controller/LoanController.java
@@ -23,7 +23,10 @@ public class LoanController {
     private LoanService loanService;
 
     @GetMapping(value = "", produces = "application/json")
-    public ResponseEntity<List<LoanResponseBody>> getAllLoansByUserIdAndSorted(@PathVariable("userId") Integer userId, @RequestParam OrderBy orderBy) {
+    public ResponseEntity<List<LoanResponseBody>> getAllLoansByUserIdAndSorted(
+            @PathVariable("userId") Integer userId,
+            @RequestParam OrderBy orderBy)
+    {
         List<Loan> loans = loanService.getAll(userId, orderBy);
         List<LoanResponseBody> loanResponseBodies = new ArrayList<>();
 
@@ -32,25 +35,38 @@ public class LoanController {
     }
 
     @PostMapping(value = "", consumes = "application/json")
-    public ResponseEntity<Void> createLoan(@PathVariable("userId") Integer userId, @Valid @RequestBody LoanRequestBody loanRequestBody) {
+    public ResponseEntity<Void> createLoan(
+            @PathVariable("userId") Integer userId,
+            @Valid @RequestBody LoanRequestBody loanRequestBody)
+    {
         loanService.addLoan(userId, new Loan(userId, loanRequestBody));
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{loanId}", produces = "application/json")
-    private ResponseEntity<LoanResponseBody> getLoanById(@PathVariable("userId") Integer userId, @PathVariable("loanId") Integer loanId) {
+    private ResponseEntity<LoanResponseBody> getLoanById(
+            @PathVariable("userId") Integer userId,
+            @PathVariable("loanId") Integer loanId)
+    {
         Loan loan = loanService.getLoan(userId, loanId);
         return new ResponseEntity<>(new LoanResponseBody(loan), HttpStatus.OK);
     }
 
     @PutMapping(value = "/{loanId}", consumes = "application/json")
-    public ResponseEntity<Void> updateLoanById(@PathVariable("userId") Integer userId, @PathVariable("loanId") Integer loanId, @Valid @RequestBody LoanRequestBody loanRequestBody) {
+    public ResponseEntity<Void> updateLoanById(
+            @PathVariable("userId") Integer userId,
+            @PathVariable("loanId") Integer loanId,
+            @Valid @RequestBody LoanRequestBody loanRequestBody)
+    {
         loanService.updateLoan(userId, loanId, new Loan(userId, loanRequestBody));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/{loanId}")
-    public ResponseEntity<Void> deleteLoanById(@PathVariable("userId") Integer userId, @PathVariable("loanId") Integer loanId) {
+    public ResponseEntity<Void> deleteLoanById(
+            @PathVariable("userId") Integer userId,
+            @PathVariable("loanId") Integer loanId)
+    {
         loanService.deleteLoan(userId, loanId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
