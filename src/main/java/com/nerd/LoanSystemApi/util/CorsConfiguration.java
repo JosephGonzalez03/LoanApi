b6deb2084149192bc278501411267f78d3601bc8
JosephGonzalez03 <josephdg3@gmail.com>
2021-11-15 17:35:29 -0800

    fix: Try allowing any port on CORS

--- src/main/java/com/nerd/LoanSystemApi/util/CorsConfiguration.java
+++ src/main/java/com/nerd/LoanSystemApi/util/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
