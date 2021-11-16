8ce6c9e8aca5df4df07b705e86c4f7f117d338e6
JosephGonzalez03 <josephdg3@gmail.com>
2021-11-16 09:25:04 -0800

    Revert "fix: Try adding reverse origin to CORS"
    This reverts commit ad8ff54e8bdf0d4ce76b90dc710dbbe675079258.

--- src/main/java/com/nerd/LoanSystemApi/util/CorsConfiguration.java
+++ src/main/java/com/nerd/LoanSystemApi/util/CorsConfiguration.java
@@ -16,7 +16,6 @@ public class CorsConfiguration
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("http://localhost:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
