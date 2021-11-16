ad8ff54e8bdf0d4ce76b90dc710dbbe675079258
JosephGonzalez03 <josephdg3@gmail.com>
2021-11-16 09:12:33 -0800

    fix: Try adding reverse origin to CORS

--- src/main/java/com/nerd/LoanSystemApi/util/CorsConfiguration.java
+++ src/main/java/com/nerd/LoanSystemApi/util/CorsConfiguration.java
@@ -16,6 +16,7 @@ public class CorsConfiguration
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
